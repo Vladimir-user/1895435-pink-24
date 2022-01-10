cb1c5d2cc7a3bd6201ac1620802dd089958dc73c
Vladimir-user <iv_vs@mail.ru>
2022-01-10 23:54:31 +0300

    Декоративные элементы главной страницы

--- source/js/load.js
+++ source/js/load.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const pageHeader = document.querySelector('.page-header');
+const headerToggle = document.querySelector('.page-header__toggle');
 const mainMenu = document.querySelector('.main-menu');
 const iconOpen = document.querySelector('.page-header__icon-open');
 const iconClose = document.querySelector('.page-header__icon-close');
 
-mainMenu.classList.toggle('hidden');
-iconOpen.classList.toggle('hidden');
+iconOpen.classList.toggle('page-header__icon-open--active');
+mainMenu.classList.toggle('main-menu--close');
 pageHeader.classList.toggle('page-header--light');
+
+headerToggle.addEventListener('click', function() {
+  pageHeader.classList.toggle('page-header--light');
+  iconOpen.classList.toggle('page-header__icon-open--active');
+  iconClose.classList.toggle('page-header__icon-close--active');
+  mainMenu.classList.toggle('main-menu--close');
+})
