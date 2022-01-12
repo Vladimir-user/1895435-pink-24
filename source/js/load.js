fd93c79b22e19adf10bd65bcdc1c5e0d923485b0
Vladimir-user <iv_vs@mail.ru>
2022-01-12 13:55:04 +0300

    Декоративные элементы главной страницы

--- source/js/load.js
+++ source/js/load.js
@@ -3,16 +3,16 @@
 const pageHeader = document.querySelector('.page-header');
 const headerToggle = document.querySelector('.page-header__toggle');
 const mainMenu = document.querySelector('.main-menu');
-const iconOpen = document.querySelector('.page-header__icon-open');
-const iconClose = document.querySelector('.page-header__icon-close');
 
-iconOpen.classList.toggle('page-header__icon-open--active');
+headerToggle.classList.toggle('page-header__toggle--active');
 mainMenu.classList.toggle('main-menu--close');
+pageHeader.classList.toggle('page-header--absolute');
 pageHeader.classList.toggle('page-header--light');
 
-headerToggle.addEventListener('click', function() {
+headerToggle.addEventListener('click', function () {
   pageHeader.classList.toggle('page-header--light');
-  iconOpen.classList.toggle('page-header__icon-open--active');
-  iconClose.classList.toggle('page-header__icon-close--active');
+  headerToggle.classList.toggle('page-header__toggle--active');
+  headerToggle.classList.toggle('page-header__toggle--close');
   mainMenu.classList.toggle('main-menu--close');
 })
+
